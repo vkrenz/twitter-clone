72309f8e675089db9072d436256e46e6b32c6193
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-29 12:10:53 -0400

    Create FeedView.swift

---
+++ Core/Feed/Views/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-09-29.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("Feed View")
+    }
+}
+
+#Preview {
+    FeedView()
+}
