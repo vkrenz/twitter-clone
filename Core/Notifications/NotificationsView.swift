ece235c6fd7a69f9b656459327c4e3b0687efbfc
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-29 15:43:30 -0400

    Create NotificationsView.swift

---
+++ Core/Notifications/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-09-29.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Hello, Notifications View")
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
