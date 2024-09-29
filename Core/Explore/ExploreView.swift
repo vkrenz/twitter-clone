754a4f279d9d42ef09e57e89ae28fdc9a08ffac8
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-29 15:38:57 -0400

    Create ExploreView.swift

---
+++ Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-09-29.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Hello, Explore View")
+    }
+}
+
+#Preview {
+    ExploreView()
+}
