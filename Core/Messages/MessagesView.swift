df0522ba2abe01af4bfd6123dedffb88242240d1
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-29 15:44:16 -0400

    Create MessagesView.swift

---
+++ Core/Messages/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-09-29.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Hello, Messages View")
+    }
+}
+
+#Preview {
+    MessagesView()
+}
