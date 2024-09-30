6916f732675e9659d16c53c248b77613822623d6
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-30 19:31:46 -0400

    Create SideMenuRowView.swift

---
+++ Core/SideMenu/Views/SideMenuRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuRowView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-09-30.
+//
+
+import SwiftUI
+
+struct SideMenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuRowView()
+}
