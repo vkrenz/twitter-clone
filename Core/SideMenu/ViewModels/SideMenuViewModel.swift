b859af9f8739afc0795c60bb4db5f276b134d35a
✧ Victor ✧ <vkrenzel@outlook.com>
2024-09-30 19:38:27 -0400

    Update SideMenuViewModel.swift

--- Core/SideMenu/ViewModels/SideMenuViewModel.swift
+++ Core/SideMenu/ViewModels/SideMenuViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum SideMenuViewModel: Int, CaseIterable {
+    case Profile
+    case Lists
+    case Bookmarks
+    case Logout
+    
+    var title: String {
+        switch self {
+        case .Profile:
+            return "Profile"
+        case .Lists:
+            return "Lists"
+        case .Bookmarks:
+            return "Bookmarks"
+        case .Logout:
+            return "Logout"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .Profile:
+            return "person"
+        case .Lists:
+            return "list.bullet"
+        case .Bookmarks:
+            return "bookmark"
+        case .Logout:
+            return "arrow.left.square"
+        }
+    }
+}
