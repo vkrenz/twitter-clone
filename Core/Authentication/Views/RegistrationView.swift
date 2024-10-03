33dd84c80e6eaa575a5dd21784767b6d4bed0524
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-02 23:03:00 -0400

    Create RegistrationView.swift

---
+++ Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-10-02.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
