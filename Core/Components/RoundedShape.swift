79268929ce04be74967a1ad2aad33a251c7bb87a
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-02 23:21:20 -0400

    Update RoundedShape.swift

--- Core/Components/RoundedShape.swift
+++ Core/Components/RoundedShape.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        return Path(path.cgPath)
+    }
+}
