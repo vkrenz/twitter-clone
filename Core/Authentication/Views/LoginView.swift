0c9377578c145901c8d38ca1072b7266cbc61b83
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-04 23:56:50 -0400

    Update LoginView.swift

--- Core/Authentication/Views/LoginView.swift
+++ Core/Authentication/Views/LoginView.swift
@@ -45,12 +45,24 @@ struct LoginView: View {
                     Text("Forgot Password?")
                         .font(.caption)
                         .fontWeight(.semibold)
-                        .foregroundColor(.blue)
+                        .foregroundColor(Color(.systemBlue))
                         .padding(.top)
                         .padding(.trailing, 24)
                 }
-
             }
+            
+            Button(action: {
+                // TODO: Handle Sign In
+            }, label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            })
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y:0)
             
             Spacer()
         }
