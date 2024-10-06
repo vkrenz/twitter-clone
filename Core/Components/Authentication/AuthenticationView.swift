b049be7d8fd72b8c0ce9a4363260bf2b5f1b45f5
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-06 15:45:31 -0400

    Create AuthenticationView.swift

---
+++ Core/Components/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-10-06.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
