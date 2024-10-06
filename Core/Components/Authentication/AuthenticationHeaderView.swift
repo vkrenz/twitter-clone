7042c9ead48bf18d4446c02a6b363b4402fed8c2
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-06 15:47:00 -0400

    Create AuthenticationHeaderView.swift

---
+++ Core/Components/Authentication/AuthenticationHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationHeaderView.swift
+//  Twitter
+//
+//  Created by ☠️ Vicc on 2024-10-06.
+//
+
+import SwiftUI
+
+struct AuthenticationHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationHeaderView()
+}
