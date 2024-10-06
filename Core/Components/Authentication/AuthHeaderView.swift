15a5cdeacac3bb32d2bf12ef9ed80237d03f7d17
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-06 15:48:34 -0400

    Update AuthHeaderView.swift

--- Core/Components/Authentication/AuthHeaderView.swift
+++ Core/Components/Authentication/AuthHeaderView.swift
@@ -1,5 +1,5 @@
 //
-//  AuthenticationHeaderView.swift
+//  AuthHeaderView.swift
 //  Twitter
 //
 //  Created by ☠️ Vicc on 2024-10-06.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct AuthenticationHeaderView: View {
+struct AuthHeaderView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AuthenticationHeaderView()
+    AuthHeaderView()
 }
