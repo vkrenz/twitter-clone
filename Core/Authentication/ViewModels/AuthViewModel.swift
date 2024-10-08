962b59a8ed311844997c77bc50aef74fe917b1ca
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-07 23:25:48 -0400

    Update AuthViewModel.swift

--- Core/Authentication/ViewModels/AuthViewModel.swift
+++ Core/Authentication/ViewModels/AuthViewModel.swift
@@ -5,4 +5,13 @@
 //  Created by ☠️ Vicc on 2024-10-07.
 //
 
-import Foundation
+import SwiftUI
+import FirebaseAuth
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+}
