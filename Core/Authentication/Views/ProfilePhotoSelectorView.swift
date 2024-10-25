665cb6de6fc946c8ffa75ac0720cb3ca12375f93
✧ Victor ✧ <vkrenzel@outlook.com>
2024-10-24 23:31:32 -0400

    Update ProfilePhotoSelectorView.swift

--- Core/Authentication/Views/ProfilePhotoSelectorView.swift
+++ Core/Authentication/Views/ProfilePhotoSelectorView.swift
@@ -12,6 +12,28 @@ struct ProfilePhotoSelectorView: View {
         VStack {
             AuthHeaderView(top_title: "Create your account", bottom_title: "Select a profile photo", authHeaderViewVariant: .Default)
             
+            Button(action: {
+                // TODO: Handle Add Photo
+                print("Pick an image")
+            }, label: {
+                ZStack {
+                    Circle()
+                        .stroke(Color("LightBlue"), lineWidth: 4)
+                    VStack(spacing: 20) {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Text("Photo")
+                            .bold()
+                            .font(.title)
+                    }
+                    .foregroundColor(Color("LightBlue"))
+                }
+                .scaledToFill()
+                .frame(width: 180, height: 180)
+                .padding(.top, 44)
+            })
+            
             Spacer()
         }
         .ignoresSafeArea()
